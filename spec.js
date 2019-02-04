2d5b3ef8c2148b5ac8da5b3faf7c903f30d1fe49
Eric P. Katz <ericpkatz@gmail.com>
2019-02-04 17:02:16 -0500

    commit

--- spec.js
+++ spec.js
@@ -12,7 +12,7 @@ after(async()=> await server.close());
 beforeEach(async()=> {
     browser = await puppeteer.launch({
       headless: true,
-      args: ['--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox'],
+      args: ['--shm-size=1gb', '--no-sandbox', '--disable-setuid-sandbox'],
     });
     page = await browser.newPage();
 
