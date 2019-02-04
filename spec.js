e2c0288a299231db43e30e804468b43a9e1f9b38
Eric P. Katz <ericpkatz@gmail.com>
2019-02-04 14:10:51 -0500

    commit

--- spec.js
+++ spec.js
@@ -9,7 +9,12 @@ after(async()=> await server.close());
 
 describe('my favorite app', ()=> {
   it('works', async()=> {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+    // will greatly affect the results
+    headless: true,
+    // important for running on various server where root user is present
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
     const page = await browser.newPage();
     /*
     await page.goto(`http://localhost:${process.env.PORT}`);
