84811ff5f6f474c75a4a42bab34afe85db154c81
Eric P. Katz <ericpkatz@gmail.com>
2019-02-04 16:37:44 -0500

    commit

--- spec.js
+++ spec.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const puppeteer = require('puppeteer');
 const app = require('./app');
+
 let server, page, browser;
 
 before(async()=> {
@@ -10,11 +11,9 @@ after(async()=> await server.close());
 
 beforeEach(async()=> {
     browser = await puppeteer.launch({
-    // will greatly affect the results
-    headless: true,
-    // important for running on various server where root user is present
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     page = await browser.newPage();
 
 });
@@ -28,9 +27,9 @@ describe('my favorite app', ()=> {
   it('works', async()=> {
     await page.goto(`http://localhost:${process.env.PORT}`);
     await page.waitForSelector('h1');
-    await page.waitForSelector('button');
-    //let html = await page.$eval('h1', el=> el.innerHTML);
-    //expect(html).to.equal('Hello World!');
+    let html = await page.$eval('h1', el=> el.innerHTML);
+    expect(html).to.equal('Hello World!');
+    await page.waitForSelector('#my-button');
     await page.click('#my-button');
     html = await page.$eval('h1', el=> el.innerHTML);
     expect(html).to.equal('HELLO WORLD!');
